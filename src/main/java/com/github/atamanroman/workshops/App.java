12d7a166b3a2493afab95f8c1d737f168930cfd8
Roman Ataman <roman.ataman@adorsys.de>
2019-03-19 00:49:24 +0100

    Create empty mvn java 12 project
    `mvn archetype:generate`

---
+++ src/main/java/com/github/atamanroman/workshops/App.java
@@ -0,0 +1,10 @@
+package com.github.atamanroman.workshops;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
