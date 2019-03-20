906b61fb424c770b204bac267639e16253342fe1
Roman Ataman <roman.ataman@adorsys.de>
2019-03-20 02:36:19 +0100

    Create tables with flyway on h2

--- src/main/java/com/github/atamanroman/workshops/banking/infrastructure/WebApplication.java
+++ src/main/java/com/github/atamanroman/workshops/banking/infrastructure/WebApplication.java
@@ -1,11 +1,24 @@
 package com.github.atamanroman.workshops.banking.infrastructure;
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletHandler;
+import org.flywaydb.core.Flyway;
 
 public class WebApplication {
 
+  private static HikariDataSource dataSource;
+
   public static void main(String[] args) throws Exception {
+
+    dataSource = new HikariDataSource();
+    dataSource.setJdbcUrl("jdbc:h2:mem:banking;DB_CLOSE_DELAY=-1");
+    dataSource.setUsername("admin");
+    dataSource.setPassword("secret");
+
+    Flyway flyway = Flyway.configure().dataSource(dataSource).load();
+    flyway.migrate();
+
     var server = new Server(8080);
     var servletHandler = new ServletHandler();
     servletHandler.addServletWithMapping(AccountServlet.class, "/accounts/*");
---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,19 @@
+CREATE TABLE accounts (
+  owner varchar(255) not null,
+  iban varchar(255) not null,
+  currency varchar(255) not null,
+  saldo integer not null
+);
+
+CREATE TABLE transactions (
+  id uuid primary key,
+  debtor_name varchar(255) not null,
+  debtor_iban varchar(255) not null,
+  creditor_name varchar(255) not null,
+  creditor_iban varchar(255) not null,
+  booking_date date not null,
+  amount integer not null,
+  currency varchar(255) not null,
+  account varchar(255) not null,
+  foreign key(account) references accounts(iban)
+);
