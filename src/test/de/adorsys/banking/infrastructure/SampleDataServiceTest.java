370b20d36f2e44ba892b4afdd3901363754e425d
Sebastian Wild <swi@adorsys.de>
2019-04-05 15:56:06 +0200

    Add SpringBootTest against database

---
+++ src/test/de/adorsys/banking/infrastructure/SampleDataServiceTest.java
@@ -0,0 +1,27 @@
+package de.adorsys.banking.infrastructure;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertNotNull;
+
+import de.adorsys.banking.domain.BankingService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SampleDataServiceTest {
+
+  @Autowired
+  private BankingService bankingService;
+
+  @Test
+  public void accountSizeTest(){
+    assertNotNull(bankingService);
+
+    assertThat(bankingService.readAccounts(), hasSize(3));
+  }
+}
